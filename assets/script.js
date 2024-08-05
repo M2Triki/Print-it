bcc9a0b3a7ab33003a4362e43af8adba5694d669
Matheo TRIKI <matheo.triki@gmail.com>
2024-08-05 19:36:15 +0200

    Modification scriptjs avec les IF et Else. Supression des dots clickables

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,8 @@
 const slides = [
-    {
-        "image": "slide1.jpg",
-        "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
-    },
-    {
-        "image": "slide2.jpg",
-        "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-    },
-    {
-        "image": "slide3.jpg",
-        "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
-    },
-    {
-        "image": "slide4.png",
-        "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
-    }
+    { "image": "slide1.jpg", "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>" },
+    { "image": "slide2.jpg", "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>" },
+    { "image": "slide3.jpg", "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>" },
+    { "image": "slide4.png", "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>" }
 ];
 
 let currentSlide = 0;
@@ -25,17 +13,12 @@ const arrowLeft = document.querySelector('.arrow_left');
 const arrowRight = document.querySelector('.arrow_right');
 const dotsContainer = document.querySelector('.dots');
 
-// Créer les bullet points
 slides.forEach((_, index) => {
     const dot = document.createElement('span');
     dot.classList.add('dot');
     if (index === 0) {
         dot.classList.add('dot_selected');
     }
-    dot.addEventListener('click', () => {
-        currentSlide = index;
-        updateSlide(currentSlide);
-    });
     dotsContainer.appendChild(dot);
 });
 
@@ -48,19 +31,33 @@ function updateSlide(index) {
 function updateDots(index) {
     const dots = document.querySelectorAll('.dot');
     dots.forEach((dot, idx) => {
-        dot.classList.toggle('dot_selected', idx === index);
+        if (idx === index) {
+            dot.classList.add('dot_selected');
+        } 
+        else {
+            dot.classList.remove('dot_selected');
+        }
     });
 }
 
 arrowLeft.addEventListener('click', () => {
-    currentSlide = (currentSlide > 0) ? currentSlide - 1 : slides.length - 1;
+    if (currentSlide > 0) {
+        currentSlide = currentSlide - 1;
+    } 
+    else {
+        currentSlide = slides.length - 1;
+    }
     updateSlide(currentSlide);
 });
 
 arrowRight.addEventListener('click', () => {
-    currentSlide = (currentSlide < slides.length - 1) ? currentSlide + 1 : 0;
+    if (currentSlide < slides.length - 1) {
+        currentSlide = currentSlide + 1;
+    } 
+    else {
+        currentSlide = 0;
+    }
     updateSlide(currentSlide);
 });
 
-// Initialiser la première diapositive
 updateSlide(currentSlide);
